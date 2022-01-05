bb8b4d1e1bb2ce97d237ddf7a44715de6046d5e1
Marat Zabirov <maratn566ek@gmail.com>
2022-01-05 14:54:39 +0300

    lock free stack benchmark

---
+++ lf-stack-bench.cpp
@@ -0,0 +1,52 @@
+#include <benchmark/benchmark.h>
+
+// static void BM_EasyFunction(benchmark::State& state) {
+//     // Perform setup here
+//     for (auto _ : state) {
+//         // This code gets timed
+//         [](){
+//             double f_multi = 0.0;
+//             for (int i = 0; i < 1000000; ++i) {
+//                 f_multi *= i;
+//             }
+//         };
+//     }
+// }
+// Register the function as a benchmark
+// BENCHMARK(BM_EasyFunction);
+
+#include <iostream>
+#include "threads-utils.hpp"
+#include "lf-stack.hpp"
+
+static void BM_LFStack(benchmark::State& state) {
+    // Perform setup here
+    for (auto _ : state) {
+        lockFreeStack<size_t> lfStack;
+        std::atomic<size_t> res = 0;
+
+        auto l_func_load = [&lfStack, &res](size_t i = 0, size_t max = 0)
+        {
+            // std::cout << "func load " << i << " " << max << std::endl;
+            size_t result = 0;
+            size_t temp_res = 0;
+            for (; i < max; i++)
+            {
+                lfStack.push(1);
+                if (i % 2 == 0)
+                {
+                    lfStack.pop(temp_res);
+                    result += temp_res;
+                }
+            }
+            res += result;
+        };
+
+        size_t count = 12'000'000;
+        l_thread_data<decltype(l_func_load)>(l_func_load, count);
+        std::cout << "Result " << res << std::endl;
+    }
+}
+BENCHMARK(BM_LFStack);
+// Run the benchmark
+BENCHMARK_MAIN();
--- threads-utils.hpp
+++ threads-utils.hpp
@@ -8,6 +8,7 @@ template <typename TLoadFunc>
 auto l_thread_data = [](TLoadFunc func_load, size_t block_count = 1, size_t add_threads = 0)
 {
     std::vector<std::future<void>> v_futures;
+    std::mutex m_cout;
     /// create several transactions
     auto conc_threads = std::thread::hardware_concurrency();
     conc_threads += add_threads;
@@ -15,15 +16,23 @@ auto l_thread_data = [](TLoadFunc func_load, size_t block_count = 1, size_t add_
     auto interval = block_count / conc_threads;
     for (size_t ith = 0; ith < conc_threads; ++ith)
     {
-        v_futures.emplace_back(std::move(std::async(std::launch::async, [=, &func_load]()
+        v_futures.emplace_back(std::move(std::async(std::launch::async, [=, &m_cout, &func_load]()
                                                     {
                                                         size_t i = ith * interval;
                                                         auto max_count = i + interval;
-                                                        std::cout << "Iter threads: " << ith << " Interval start: " << i << " Interval end: " << max_count << std::endl;
+                                                        if ((ith + 1) == conc_threads)
+                                                            max_count = block_count;
+                                                        {
+                                                            std::lock_guard<std::mutex> lock(m_cout);
+                                                            std::cout << "Iter threads: " << ith << " Interval start: " << i << " Interval end: " << max_count << std::endl;
+                                                        }
                                                         func_load(i, max_count);
                                                     })));
     }
-    std::cout << "Wait futures" << std::endl;
+    {
+        std::lock_guard<std::mutex> lock(m_cout);
+        std::cout << "Wait futures" << std::endl;
+    }
     for (auto &it_future : v_futures)
     {
         it_future.get();
