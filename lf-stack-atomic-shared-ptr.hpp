2f2c33bd4c9b68278db0435d535aba1b7d31157c
Marat Zabirov <maratn566ek@gmail.com>
2022-01-05 19:43:52 +0300

    lock free stack with atomic shared ptr

---
+++ lf-stack-atomic-shared-ptr.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include <atomic>
+#include <memory>
+
+template<typename T>
+class lockFreeStack
+{
+    struct node;
+    using TAtomicSharedPtr = std::atomic<std::shared_ptr<node>>;
+private:
+    struct node
+    {
+        std::shared_ptr<T> data;
+        TAtomicSharedPtr next;
+        node(T const& data_):
+            data(std::make_shared<T>(data_))
+        {}
+    };
+    TAtomicSharedPtr head;
+public:
+    void push(T const& data)
+    {
+        std::shared_ptr<node> const new_node=std::make_shared<node>(data);
+        new_node->next=head.load();
+        while(!head.compare_exchange_weak(new_node->next,new_node));
+    }
+    std::shared_ptr<T> pop()
+    {
+        std::shared_ptr<node> old_head=head.load();
+        while(old_head && !head.compare_exchange_weak(
+                  old_head,old_head->next.load()));
+        if(old_head) {
+            old_head->next=std::shared_ptr<node>();
+            return old_head->data;
+        }
+        
+        return std::shared_ptr<T>();
+    }
+    ~lockFreeStack(){
+        while(pop());
+    }
+};
